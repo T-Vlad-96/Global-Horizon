b711dff53d898fea3ca421201d3ca4e2b7295606
user <user@MacBook-Air-user.local>
2025-08-16 15:19:45 +0300

    refactor: remove unnessesary comments out from models.py

--- newspapers_tracker/models.py
+++ newspapers_tracker/models.py
@@ -3,7 +3,6 @@ from django.core.validators import MaxValueValidator
 from django.db import models
 
 
-# Create your models here.
 class Topic(models.Model):
     name = models.CharField(max_length=255, unique=True)
 
