214506e10e4230e72ab0796a3ed42661ed74feb9
user <user@MacBook-Air-user.local>
2025-10-05 19:02:18 +0300

    test: Add test_topic_update_view_public
    - Ensure an access to topic_update view for authenticated users only
    - Ensure that unauthenticated users are redirected to 'login' page
    - fix syntax mistake in topic urls constant variables definition

--- newspapers_tracker/tests/topic_views_tests.py
+++ newspapers_tracker/tests/topic_views_tests.py
@@ -1,13 +1,19 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
 
+from newspapers_tracker.models import Topic
+
 TOPIC_LIST_VIEW_URL = reverse_lazy("newspapers_tracker:topic-list")
 TOPIC_CREATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-create")
-TOPIC_UPDATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-update", pk=1)
-TOPIC_DELETE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-delete", pk=1)
+TOPIC_UPDATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-update", kwargs={"pk": 1})
+TOPIC_DELETE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-delete", kwargs={"pk": 1})
 
 
 class TopicViewsPublicTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.topic = Topic.objects.create(name="test_topic")
+
     def test_topic_list_view_public(self):
         response = self.client.get(TOPIC_LIST_VIEW_URL)
         self.assertNotEquals(
@@ -28,4 +34,15 @@ class TopicViewsPublicTests(TestCase):
         self.assertRedirects(
             response,
             reverse_lazy("login") + "?next=/topics/create/"
+        )
+
+    def test_topic_update_view_public(self):
+        response = self.client.get(TOPIC_UPDATE_VIEW_URL)
+        self.assertNotEquals(
+            response.status_code,
+            200
+        )
+        self.assertRedirects(
+            response,
+            reverse_lazy("login") + "?next=/topics/update/1/"
         )
