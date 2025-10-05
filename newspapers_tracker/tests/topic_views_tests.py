deadaf46cd15a03ad190aae003eb0b8f84a83fbc
user <user@MacBook-Air-user.local>
2025-10-05 18:43:06 +0300

    test: Add new file topic_views_tests.py to store all tests related to Topic model views
    - Add constant variables to store all the urls to Topic model views
    - Add class TopicViewsPublicTests to store the tests that verifies an access to Topic model views for authenticated users only
    - Add test_topic_list_view_public that asserts the access to topic_list view for authenticated users only
    - Ensure that unauthenticated users are redirected to 'login' page

---
+++ newspapers_tracker/tests/topic_views_tests.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.urls import reverse_lazy
+
+
+TOPIC_LIST_VIEW_URL = reverse_lazy("newspapers_tracker:topic-list")
+TOPIC_CREATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-create")
+TOPIC_UPDATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-update", pk=1)
+TOPIC_DELETE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-delete", pk=1)
+
+
+class TopicViewsPublicTests(TestCase):
+    def test_topic_list_view_public(self):
+        response = self.client.get(TOPIC_LIST_VIEW_URL)
+        self.assertNotEquals(
+            response.status_code,
+            200
+        )
+        self.assertRedirects(
+            response,
+            reverse_lazy("login") + "?next=/topics/"
+        )
