900b71c7bdefcfe50fd57ddd61f6db80f84ad815
user <user@MacBook-Air-user.local>
2025-10-05 19:13:30 +0300

    test: Add TopicListViewPrivateTests class to store the tests for topic_list view
    - Add setUpTestData classmethod to create 7 Topic instances for tests and user instance
    - Add setUp method to login the user
    - Add test_topic_list_view_access method to ensure an access to topic_list view for authenticated user

--- newspapers_tracker/tests/topic_views_tests.py
+++ newspapers_tracker/tests/topic_views_tests.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse_lazy
 
@@ -56,4 +57,25 @@ class TopicViewsPublicTests(TestCase):
         self.assertRedirects(
             response,
             reverse_lazy("login") + "?next=/topics/delete/1/"
+        )
+
+
+class TopicListViewPrivateTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        for i in range(7):
+            Topic.objects.create(name=f"topic_{i}")
+        cls.user = get_user_model().objects.create_user(
+            username="test_user",
+            password="Abc12345"
+        )
+
+    def setUp(self):
+        self.client.force_login(self.user)
+
+    def test_topic_list_view_access(self):
+        response = self.client.get(TOPIC_LIST_VIEW_URL)
+        self.assertEqual(
+            response.status_code,
+            200
         )
