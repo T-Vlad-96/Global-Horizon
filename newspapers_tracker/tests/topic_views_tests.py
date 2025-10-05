f4f8c8289b5f2e6107cc5773502e830d7a3e720e
user <user@MacBook-Air-user.local>
2025-10-05 18:49:22 +0300

    feat: Add test_topic_create_view_public
    - Ensure the access to topic_create view for authenticated users only
    - Ensure that unauthenticated users are redirected to 'login' page

--- newspapers_tracker/tests/topic_views_tests.py
+++ newspapers_tracker/tests/topic_views_tests.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-
 TOPIC_LIST_VIEW_URL = reverse_lazy("newspapers_tracker:topic-list")
 TOPIC_CREATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-create")
 TOPIC_UPDATE_VIEW_URL = reverse_lazy("newspapers_tracker:topic-update", pk=1)
@@ -18,4 +17,15 @@ class TopicViewsPublicTests(TestCase):
         self.assertRedirects(
             response,
             reverse_lazy("login") + "?next=/topics/"
+        )
+
+    def test_topic_create_view_public(self):
+        response = self.client.get(TOPIC_CREATE_VIEW_URL)
+        self.assertNotEquals(
+            response.status_code,
+            200
+        )
+        self.assertRedirects(
+            response,
+            reverse_lazy("login") + "?next=/topics/create/"
         )
