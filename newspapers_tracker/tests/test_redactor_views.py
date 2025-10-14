5c5c5397e18e3e64ea8db6f0bcc2921540ab78f7
user <user@MacBook-Air-user.local>
2025-10-14 13:15:35 +0300

    refactor: Remove unnessary import from test_redactor_views.py

--- newspapers_tracker/tests/test_redactor_views.py
+++ newspapers_tracker/tests/test_redactor_views.py
@@ -2,8 +2,6 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse_lazy
 
-from newspapers_tracker.models import Redactor
-
 REDACTOR_LIST_VIEW_URL = reverse_lazy("newspapers_tracker:redactor-list")
 REDACTOR_CREATE_VIEW_URL = reverse_lazy("newspapers_tracker:redactor-create")
 REDACTOR_DETAIL_VIEW_URL = reverse_lazy(
