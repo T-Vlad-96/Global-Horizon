ebcbe998d73f8b43b39de0804d08dc0ccaa4f06b
user <user@MacBook-Air-user.local>
2025-10-14 14:47:42 +0300

    test: Add file test_newspaper_views.py to store the tests for Newspaper model views
    - Add constants for every single url path to newspapers views
    - Add class NewspaperViewsPublicTests as a sublclass of TestCase to store the tests that ensure the access to Newspaper model views for authenticated users only
    - Add setUpTestData classmethod to create newspaper instance for tests
    - Add test_newspaper_list_view_access_public to ensure the access to newspaper_list view for authenticated users only

---
+++ newspapers_tracker/tests/test_newspaper_views.py
@@ -0,0 +1,43 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse_lazy
+
+from newspapers_tracker.models import Newspaper, Topic
+
+NEWSPAPER_LIST_URL = reverse_lazy("newspapers_tracker:newspaper-list")
+NEWSPAPER_CREATE_URL = reverse_lazy("newspapers_tracker:newspaper-create")
+NEWSPAPER_DETAIL_URL = reverse_lazy(
+    "newspapers_tracker:newspaper-detail",
+    kwargs={"pk": 1}
+)
+NEWSPAPER_UPDATE_URL = reverse_lazy(
+    "newspapers_tracker:newspaper-update",
+    kwargs={"pk": 1}
+)
+NEWSPAPER_DELETE_URL = reverse_lazy(
+    "newspapers_tracker:newspaper-delete",
+    kwargs={"pk": 2}
+)
+
+
+class NewspaperViewsPublicTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.topic = Topic.objects.create(name="test_topic")
+        cls.publisher = get_user_model().objects.create_user(
+            username="test_username",
+            password="Abc12345"
+        )
+        cls.newspaper = Newspaper.objects.create(
+            title="test_title",
+            topic=cls.topic,
+        )
+        cls.newspaper.publishers.add(cls.publisher)
+
+    def test_newspaper_list_view_access_public(self):
+        response = self.client.get(NEWSPAPER_LIST_URL)
+        self.assertNotEquals(
+            response.status_code,
+            200,
+            msg="Access for authenticated users only"
+        )
