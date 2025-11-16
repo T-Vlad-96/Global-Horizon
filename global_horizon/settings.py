9d4e55e9e02a133479deee293aba9ac396367238
user <user@MacBook-Air-user.local>
2025-11-16 23:07:14 +0200

    add defaul value for SECRET_KEY

--- global_horizon/settings.py
+++ global_horizon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+from email.policy import default
 from pathlib import Path
 from decouple import config
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY")
+SECRET_KEY = config("SECRET_KEY", default="0cs7!345tezp@^tio2%^77iba*$_w=))+6tu229z!ii9qn&ilc")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
