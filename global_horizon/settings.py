fd8843d7112a587429a57a8cb4f12908dbc4ce38
user <user@MacBook-Air-user.local>
2025-12-07 16:35:50 +0200

    Add python-dotenv library and use it in settings.py to get an access to '.env' file variables

--- global_horizon/settings.py
+++ global_horizon/settings.py
@@ -9,21 +9,25 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
 from email.policy import default
 from pathlib import Path
-from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+env_path = os.path.join(BASE_DIR, ".env")
+load_dotenv(env_path)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("SECRET_KEY", default="0cs7!345tezp@^tio2%^77iba*$_w=))+6tu229z!ii9qn&ilc")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG", default=False)
+DEBUG = os.environ.get("DEBUG") != False
 
 ALLOWED_HOSTS = []
 
