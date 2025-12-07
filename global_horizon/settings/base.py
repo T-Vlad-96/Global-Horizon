1b881c5bd68de235c97842d089565cba7ff79c79
user <user@MacBook-Air-user.local>
2025-12-07 16:43:16 +0200

    refactor: Separate settings into different python files and put them inside new package 'settings'
    - Add file base.py
    - copy into base.py all the settings from settings.py and delete original settings.py file
    - Add dev.py settings configurations for development and import in it all the code from base.py
    - Add prod.py settings configurations for production and import in it all the code from base.py

--- global_horizon/settings/base.py
+++ global_horizon/settings/base.py
@@ -10,12 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
-from email.policy import default
 from pathlib import Path
 from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 env_path = os.path.join(BASE_DIR, ".env")
 load_dotenv(env_path)
