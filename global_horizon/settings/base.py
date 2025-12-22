37395e1098862c817b8c7ed44a1d333111f99cf4
user <user@MacBook-Air-user.local>
2025-12-22 23:57:30 +0200

    fix: fix typo in settings/base.py. (fix value for ASSETS_ROOT)

--- global_horizon/settings/base.py
+++ global_horizon/settings/base.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = "staticfiles"
 
-ASSETS_ROOT = "/static/"
+ASSETS_ROOT = "/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
